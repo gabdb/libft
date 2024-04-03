a3b78cd60a055f8f5c2be43778c4a468cd3de3ed
gabdb <gabrielnyssens@gmail.com>
2024-04-03 20:17:39 +0200

    first commit

---
+++ libC_functions/ft_isalpha.c
@@ -0,0 +1,21 @@
+
+
+int	ft_isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+
+/*
+#include <stdio.h>
+int main(void)
+{
+	int a = '.';
+	int result = ft_isalpha(a);
+	printf("%d", result);
+
+	return (0);
+}
+*/

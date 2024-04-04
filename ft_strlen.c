f5eb5f6b6decdfaf8797051d2006f27e8dd9f6be
Gabriel Nyssens <gnyssens@student.s19.be>
2024-04-04 16:45:55 +0200

    rearranger_dossier

---
+++ ft_strlen.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gnyssens <gnyssens@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/04 16:10:59 by gnyssens          #+#    #+#             */
+/*   Updated: 2024/04/04 16:33:23 by gnyssens         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+size_t ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+#include <stdio.h>
+int main(void)
+{
+	const char test[] = "jsp";
+	size_t result = ft_strlen(test);
+	printf("%zu\n", result);
+
+	return (0);
+}

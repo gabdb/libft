1c9f58ee9f58cce08db8882824066a87a5fbaed8
Gabriel Nyssens <gnyssens@student.s19.be>
2024-04-08 19:11:29 +0200

    premier lundi

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,37 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gnyssens <gnyssens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/08 17:08:20 by gnyssens          #+#    #+#             */
-/*   Updated: 2024/04/08 19:02:10 by gnyssens         ###   ########.fr       */
+/*   Created: 2024/04/08 18:48:17 by gnyssens          #+#    #+#             */
+/*   Updated: 2024/04/08 19:09:20 by gnyssens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strchr(const char *str, int c)
+char *ft_strrchr(const char *str, int c)
 {
 	const unsigned char	*new_str;
-	size_t				i;
+	size_t				ind;
 
-	new_str = (const unsigned char*) str;
-	i = 0;
-	while (new_str[i])
+	if (!str)
+		return (0);
+	new_str = (unsigned char*)str;
+	ind = ft_strlen(str) - 1;
+	if (c == 0 && ind >= 0)
+		return ((char *)(str + ind + 1));
+	while (ind >= 0)
 	{
-		if (c == new_str[i])
-			return ((char *)str + i);
-		i++;
+		if (c == new_str[ind])
+			return ((char *)(str + ind));
+		ind--;
 	}
-	if (c == 0)
-		return ((char *)str + i);
 	return (0);
-}
-int main (void)
-{
-	const char *str = "Ou est charlie";
-	int c = 0;
-	printf("%s", ft_strchr(str, c));
 }

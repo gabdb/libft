abd203433bf3fdc18596ca219d7cacc7bbc21ed8
Gabriel Nyssens <gnyssens@student.s19.be>
2024-04-22 13:37:00 +0200

    normalement fini

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: gnyssens <gnyssens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 12:56:20 by gnyssens          #+#    #+#             */
-/*   Updated: 2024/04/15 18:36:13 by gnyssens         ###   ########.fr       */
+/*   Updated: 2024/04/22 13:30:22 by gnyssens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ void	*ft_memset(void *s, int c, size_t n)
 	unsigned char	*p;
 	size_t			i;
 
-	if (!s)
-		return (NULL);
 	p = (unsigned char *)s;
 	i = 0;
 	while (n > 0)
